710d4757a951a7c552d159e617650fb9b0e21c32
Zack Bumpous <upwork.ztb@gmail.com>
2020-09-07 12:43:13 -0500

    Completes HackerRanks 30 day challenge number 5 in JavaScript. Adds code into a function that will take an input n and begin a loop that stores the product of n and i, i being the variable we are incrementing in our for loop. Our function finally console logs the numbers being multiplied and the result. Will multiply n by 1 through 10.

---
+++ challenge5.js
@@ -0,0 +1,37 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+
+
+// IGNORE CODE ABOVE THIS
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    for (var i = 1; i < 11; i++){
+        var result = n * i;
+        console.log(`${n} x ${i} = ${result.toString()}`)
+    }
+}
